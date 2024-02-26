0c72707cf93a4ea82bb3456cd0beb83998a8661a
root <root@Poly-S>
2024-02-26 19:36:39 +0300

    final

--- calculator.js
+++ calculator.js
@@ -1,1 +1,24 @@
+const readlineSync = require('readline-sync');
 
+const calculator = {
+  a: null,
+  b: null,
+  
+  read: function() {
+    this.a = parseFloat(readlineSync.question('Введите значение a: '));
+    this.b = parseFloat(readlineSync.question('Введите значение b: '));
+  },
+  
+  sum: function() {
+    return this.a + this.b;
+  },
+  
+  mul: function() {
+    return this.a * this.b;
+  }
+};
+
+console.log('--- Калькулятор ---');
+calculator.read();
+console.log('Сумма: ', calculator.sum());
+console.log('Произведение: ', calculator.mul());
